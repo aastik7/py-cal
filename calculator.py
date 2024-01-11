a495a23f83799dedaeef0ef549e043ad9dc02ab8
Aastik <aastikdude7@gmail.com>
2024-01-11 09:28:18 -0800

    Choice input

--- calculator.py
+++ calculator.py
@@ -21,5 +21,36 @@ def mult(a,b):
     print (str(a) + "*" + str(b) + "=" + str (answer))
 
 
+print("A. Addition \n B. Subtraction \n C. Divisoin \n D. Division \n E. Exit ")
 
-choice = input()
+choice = input("Input your choice: ")
+
+if choice == "a" or choice == "A": 
+    print ("Addition")
+    a = int(input("Input the first number = "))
+    b = int(input("Input the second number = "))
+    add(a,b)
+ 
+elif choice == "b" or choice == "B": 
+    print ("Subtraction")
+    a = int(input("Input the first number = "))
+    b = int(input("Input the second number = "))
+    sub(a,b)
+
+elif choice == "c" or choice == "C": 
+    print ("Division")
+    a = int(input("Input the first number = "))
+    b = int(input("Input the second number = "))
+    div(a,b)
+
+elif choice == "d" or choice == "D": 
+    print ("Multiplication")
+    a = int(input("Input the first number = "))
+    b = int(input("Input the second number = "))
+    mult(a,b)
+
+elif choice == "e" or choice == "E":
+    print("Exited")
+    quit() 
+
+    
