cb5422c655a004b1c50b4c18af547790fbdfb317
Aastik <aastikdude7@gmail.com>
2024-01-11 10:01:33 -0800

    added while loop to stop quit()

--- calculator.py
+++ calculator.py
@@ -21,36 +21,38 @@ def mult(a,b):
     print (str(a) + "*" + str(b) + "=" + str (answer))
 
 
-print("A. Addition \n B. Subtraction \n C. Divisoin \n D. Division \n E. Exit ")
+print(" A. Addition \n B. Subtraction \n C. Divisoin \n D. Division \n E. Exit ")
 
-choice = input("Input your choice: ")
+while True:
+    choice = input("Input your choice: ")
 
-if choice == "a" or choice == "A": 
-    print ("Addition")
-    a = int(input("Input the first number = "))
-    b = int(input("Input the second number = "))
-    add(a,b)
- 
-elif choice == "b" or choice == "B": 
-    print ("Subtraction")
-    a = int(input("Input the first number = "))
-    b = int(input("Input the second number = "))
-    sub(a,b)
-
-elif choice == "c" or choice == "C": 
-    print ("Division")
-    a = int(input("Input the first number = "))
-    b = int(input("Input the second number = "))
-    div(a,b)
-
-elif choice == "d" or choice == "D": 
-    print ("Multiplication")
-    a = int(input("Input the first number = "))
-    b = int(input("Input the second number = "))
-    mult(a,b)
-
-elif choice == "e" or choice == "E":
-    print("Exited")
-    quit() 
-
+    if choice == "a" or choice == "A": 
+        print ("Addition")
+        a = int(input("Input the first number = "))
+        b = int(input("Input the second number = "))
+        add(a,b)
     
+    elif choice == "b" or choice == "B": 
+        print ("Subtraction")
+        a = int(input("Input the first number = "))
+        b = int(input("Input the second number = "))
+        sub(a,b)
+
+    elif choice == "c" or choice == "C": 
+        print ("Division")
+        a = int(input("Input the first number = "))
+        b = int(input("Input the second number = "))
+        div(a,b)
+
+    elif choice == "d" or choice == "D": 
+        print ("Multiplication")
+        a = int(input("Input the first number = "))
+        b = int(input("Input the second number = "))
+        mult(a,b)
+
+    elif choice == "e" or choice == "E":
+        print("Exited")
+        break
+
+    else:
+        print("Invalid input: Please choose a valid option (A,B,C,D,E)")
