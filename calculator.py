c216cf9062f112be68f3c734c68ee2d81cd4047d
Aastik <aastikdude7@gmail.com>
2024-01-11 16:06:22 -0800

    defining basic calculator functions

--- calculator.py
+++ calculator.py
@@ -1,3 +1,25 @@
+#Defining basic functions
+
+#Adddition
 def add(a,b):
     answer = a+b
     print( str(a) + "+" + str(b) + "=" + str(answer) ) 
+
+#Subtraction
+def sub(a,b):
+    answer = a-b
+    print( str(a) + "-" + str(b) + "=" + str(answer))
+
+#Division
+def div(a,b):
+    answer = a/b
+    print( str(a) + "/" + str(b) + "=" + str (answer))
+
+#Multiplication
+def mult(a,b):
+    answer = a*b 
+    print (str(a) + "*" + str(b) + "=" + str (answer))
+
+
+
+choice = input()
